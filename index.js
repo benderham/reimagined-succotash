1c2db5698308d9623e5f0c3263d8191d609d8fb4
Ben Derham <ben@benderham.com>
2017-03-14 22:12:03 +0800

    reorganise function

--- index.js
+++ index.js
@@ -2,12 +2,11 @@ const fs = require('fs')
 const getColors = require('get-image-colors')
 const buffer = fs.readFileSync('image.jpg')
 
+const sortingHat = function(thingsToSort) {
+  console.log('sorting', thingsToSort)
+}
+
 getColors(buffer, 'image/jpg').then(colors => {
   const spuds = colors.map(color => color.hsl())
   return sortingHat(spuds)
 })
-
-// sorting hat - group colors
-const sortingHat = function(thingsToSort) {
-  console.log('sorting', thingsToSort)
-}
